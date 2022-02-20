5a3fba97c95076f04434871e20f011c230751de7
Jens Dietrich <jens.dietrich@gmail.com>
2022-02-20 14:07:12 +1300

    added tests for instrumentation of static field access

--- nullannoinference-agent2/src/test/java/nz/ac/wgtn/nullannoinference/agent2/TestFieldAccess.java
+++ nullannoinference-agent2/src/test/java/nz/ac/wgtn/nullannoinference/agent2/TestFieldAccess.java
@@ -2,6 +2,7 @@ package nz.ac.wgtn.nullannoinference.agent2;
 
 import com.ea.agentloader.AgentLoader;
 import nz.ac.wgtn.nullannoinference.agent2.data.A;
+import nz.ac.wgtn.nullannoinference.agent2.data.B;
 import nz.ac.wgtn.nullannoinference.commons.Issue;
 import nz.ac.wgtn.nullannoinference.commons.IssueStore;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class TestNonStaticFieldAccess {
+public class TestFieldAccess {
 
     @BeforeAll
     public static void installAgent() {
@@ -79,8 +80,70 @@ public class TestNonStaticFieldAccess {
     }
 
     @Test
-    public void testForFP() {
+    public void testNonStaticFieldForFP() {
         new A().dontResetf5();
+        assertEquals(0, IssueStore.getIssues().size());
+    }
+
+    @Test
+    public void testStaticFieldAccessString() {
+        B.resetF1();
+        assertEquals(1, IssueStore.getIssues().size());
+
+        Issue issue = IssueStore.getIssues().stream().filter(iss -> iss.getMethodName().equals("F1")).findFirst().orElse(null);
+        assertNotNull(issue);
+        assertEquals(B.class.getName(),issue.getClassName());
+        assertEquals("F1",issue.getMethodName());
+        assertEquals("Ljava/lang/String;",issue.getDescriptor());
+        assertEquals(-1,issue.getArgsIndex());
+        assertEquals(Issue.IssueType.FIELD,issue.getKind());
+    }
+
+    @Test
+    public void testStaticFieldAccessObject() {
+        B.resetF2();
+        assertEquals(1, IssueStore.getIssues().size());
+
+        Issue issue = IssueStore.getIssues().stream().filter(iss -> iss.getMethodName().equals("F2")).findFirst().orElse(null);
+        assertNotNull(issue);
+        assertEquals(B.class.getName(),issue.getClassName());
+        assertEquals("F2",issue.getMethodName());
+        assertEquals("Ljava/lang/Object;",issue.getDescriptor());
+        assertEquals(-1,issue.getArgsIndex());
+        assertEquals(Issue.IssueType.FIELD,issue.getKind());
+    }
+
+    @Test
+    public void testStaticFieldAccessArrayOfString() {
+        B.resetF3();
+        assertEquals(1, IssueStore.getIssues().size());
+
+        Issue issue = IssueStore.getIssues().stream().filter(iss -> iss.getMethodName().equals("F3")).findFirst().orElse(null);
+        assertNotNull(issue);
+        assertEquals(B.class.getName(),issue.getClassName());
+        assertEquals("F3",issue.getMethodName());
+        assertEquals("[Ljava/lang/String;",issue.getDescriptor());
+        assertEquals(-1,issue.getArgsIndex());
+        assertEquals(Issue.IssueType.FIELD,issue.getKind());
+    }
+
+    @Test
+    public void testStaticFieldAccessArrayOfInt() {
+        B.resetF4();
+        assertEquals(1, IssueStore.getIssues().size());
+
+        Issue issue = IssueStore.getIssues().stream().filter(iss -> iss.getMethodName().equals("F4")).findFirst().orElse(null);
+        assertNotNull(issue);
+        assertEquals(B.class.getName(),issue.getClassName());
+        assertEquals("F4",issue.getMethodName());
+        assertEquals("[I",issue.getDescriptor());
+        assertEquals(-1,issue.getArgsIndex());
+        assertEquals(Issue.IssueType.FIELD,issue.getKind());
+    }
+
+    @Test
+    public void testStaticFieldForFP() {
+        B.dontResetF5();
         assertEquals(0, IssueStore.getIssues().size());
     }
 }
---
+++ nullannoinference-agent2/src/test/java/nz/ac/wgtn/nullannoinference/agent2/data/B.java
@@ -0,0 +1,31 @@
+package nz.ac.wgtn.nullannoinference.agent2.data;
+
+public class B {
+
+    private static String F1 = "before";
+    private static Object F2 = "before";
+    private static String[] F3 = new String[]{"before"};
+    private static int[] F4 = new int[]{42};
+    private static String F5 = "before";
+
+    public static void resetF1() {
+        F1 = null;
+    }
+
+    public static void resetF2() {
+        F2 = null;
+    }
+
+    public static void resetF3() {
+        F3 = null;
+    }
+
+    public static void resetF4() {
+        F4 = null;
+    }
+
+    public static void dontResetF5() {
+        F5 = "";
+    }
+
+}
