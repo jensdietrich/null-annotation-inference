893dbce18dc81fe36b376e2cb74a00456bb1e514
Jens Dietrich <jens.dietrich@gmail.com>
2022-02-24 15:20:45 +1300

    added support to aggregate issues

---
+++ nullannoinference-refiner/src/main/java/nz/ac/wgtn/nullannoinference/IssueAggregator.java
@@ -0,0 +1,58 @@
+package nz.ac.wgtn.nullannoinference;
+
+import nz.ac.wgtn.nullannoinference.commons.Issue;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * An utility that takes a collection of issues, and returns a new collection of issues that is a subset of the first one,
+ * with all duplicates removed. Duplicate detection is based on the equivalence of issues defined by the following properties:
+ * className, methodName, descriptor, kind, argsIndex
+ * I.e. provenance related attributes are ignored. For instance, if two tests reveal the same issue (say the nullability of the return
+ * value of Foo::foo, then only one of them will be included in the result.
+ * No particular heuristics is used to pick the representative of the equivalence class included.
+ * @author jens dietrich
+ */
+public class IssueAggregator {
+
+    public static Collection <Issue> aggregate (Collection<Issue> issues) {
+        Map<IssueCore,Issue> index = new HashMap<>();
+        for (Issue issue:issues) {
+            IssueCore key = new IssueCore(issue.getClassName(),issue.getMethodName(),issue.getDescriptor(),issue.getKind(),issue.getArgsIndex());
+            index.put(key,issue);
+        }
+        return index.values();
+    }
+
+    static class IssueCore {
+        private String className = null;
+        private String methodName = null;
+        private String descriptor = null;
+        private Issue.IssueType kind = null;
+        private int argsIndex = -1;
+
+        public IssueCore(String className, String methodName, String descriptor, Issue.IssueType kind, int argsIndex) {
+            this.className = className;
+            this.methodName = methodName;
+            this.descriptor = descriptor;
+            this.kind = kind;
+            this.argsIndex = argsIndex;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            IssueCore issueCore = (IssueCore) o;
+            return argsIndex == issueCore.argsIndex && Objects.equals(className, issueCore.className) && Objects.equals(methodName, issueCore.methodName) && Objects.equals(descriptor, issueCore.descriptor) && kind == issueCore.kind;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(className, methodName, descriptor, kind, argsIndex);
+        }
+    }
+}
---
+++ nullannoinference-refiner/src/test/java/nz/ac/wgtn/nullannoinference/TestIssueAggregator.java
@@ -0,0 +1,42 @@
+package nz.ac.wgtn.nullannoinference;
+
+import nz.ac.wgtn.nullannoinference.commons.Issue;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestIssueAggregator {
+
+    @Test
+    public void test () {
+        Issue issue1 = new Issue("A","foo","()Ljava/lang/String","context",Issue.IssueType.ARGUMENT,1);
+        issue1.setStacktrace(List.of("foo1","bar1"));
+
+        Issue issue2 = new Issue("A","foo","()Ljava/lang/String","context",Issue.IssueType.ARGUMENT,1);
+        issue1.setStacktrace(List.of("foo2","bar2")); // different reason, otherwise same issue
+
+        Issue issue3 = new Issue("B","foo","()Ljava/lang/String","context",Issue.IssueType.ARGUMENT,0);
+        issue1.setStacktrace(List.of("foo1","bar1")); // different issue
+
+        Set<Issue> issues = Set.of(issue1,issue2,issue3);
+
+        Collection<Issue> aggregatedSet = IssueAggregator.aggregate(issues);
+
+        assertEquals(2,aggregatedSet.size());
+
+        Collection<Issue> issuesInA = aggregatedSet.stream().filter(i -> i.getClassName().equals("A")).collect(Collectors.toSet());
+        assertEquals(1,issuesInA.size());
+        assertTrue(issuesInA.contains(issue1) || issuesInA.contains(issue2));
+
+        Collection<Issue> issuesInB = aggregatedSet.stream().filter(i -> i.getClassName().equals("B")).collect(Collectors.toSet());
+        assertEquals(1,issuesInB.size());
+        assertTrue(issuesInB.contains(issue3));
+
+    }
+}
