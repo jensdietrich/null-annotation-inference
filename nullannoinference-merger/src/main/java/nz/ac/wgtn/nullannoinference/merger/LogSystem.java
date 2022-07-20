26c595eb2ace801b0d454330158e43663fb94ed1
Jens Dietrich <jens.dietrich@gmail.com>
2022-07-20 13:46:31 +1200

    added merger module (to combine issues)

---
+++ nullannoinference-merger/src/main/java/nz/ac/wgtn/nullannoinference/merger/LogSystem.java
@@ -0,0 +1,23 @@
+package nz.ac.wgtn.nullannoinference.scoper;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+
+/**
+ * Log system to be used.
+ * @author jens dietrich
+ */
+public class LogSystem {
+
+    static {
+        Configurator.initialize(new DefaultConfiguration());
+        Configurator.setRootLevel(Level.INFO);
+    }
+
+    public static Logger getLogger(String name) {
+        return LogManager.getLogger(name);
+    }
+}
