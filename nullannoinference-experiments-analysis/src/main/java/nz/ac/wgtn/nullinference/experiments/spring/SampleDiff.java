479b4a4c4b835f5a8f2f7b552a6bd4d5dc716eda
Jens Dietrich <jens.dietrich@gmail.com>
2022-08-23 10:08:56 +1200

    updated samplediff script, now also generating md files suitable for opening issues / documenting PRs

--- nullannoinference-experiments-analysis/src/main/java/nz/ac/wgtn/nullinference/experiments/spring/SampleDiff.java
+++ nullannoinference-experiments-analysis/src/main/java/nz/ac/wgtn/nullinference/experiments/spring/SampleDiff.java
@@ -3,16 +3,18 @@ package nz.ac.wgtn.nullinference.experiments.spring;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Sets;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import nz.ac.wgtn.nullannoinference.commons.*;
-import nz.ac.wgtn.nullinference.experiments.Utils;
+import nz.ac.wgtn.nullinference.experiments.descr.DescriptorParser;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
-import static nz.ac.wgtn.nullinference.experiments.spring.DataSet.SPRING_MODULES;
 
 /**
  * Sample the difference between two issue sets.
@@ -20,56 +22,137 @@ import static nz.ac.wgtn.nullinference.experiments.spring.DataSet.SPRING_MODULES
  */
 public class SampleDiff extends Experiment {
 
-    public static final File EXTRACTED_PLUS_ISSUES_FOLDER = new File("experiments-spring/results/extracted+");
-    public static final File OBSERVED_PLUS_ISSUES_FOLDER = new File("experiments-spring/results/observed+");
-    public static final File SHADING_SPECS = new File("experiments-spring/shaded.json");
+    public static final File EXTRACTED_FOLDER = new File("experiments-spring/results/extracted+/");
+    public static final File OBSERVED_FOLDER = new File("experiments-spring/results/sanitized/");
+
+    public static final String MODULE = "spring-core";
+
+    public static final int ISSUE_KERNEL_LIMIT = 3;
+    public static final int ISSUE_INSTANCE_PER_KERNEL_LIMIT = 3;
 
 
-    public static final String EXTRACTED_NOT_OBSERVED = "experiments-spring/results/diff/extracted-observed-<module>.json";
-    public static final String OBSERVED_NOT_EXTRACTED = "experiments-spring/results/diff/observed-extracted-<module>.json";
-    public static final int LIMIT = Integer.MAX_VALUE;
+    public static final Predicate<IssueKernel> ALL = issueKernel -> true;
+    public static final Predicate<IssueKernel> ARG = issueKernel -> issueKernel.getKind()== Issue.IssueType.ARGUMENT;
+    public static final Predicate<IssueKernel> RET = issueKernel -> issueKernel.getKind()== Issue.IssueType.RETURN_VALUE;
+    public static final Predicate<IssueKernel> FLD = issueKernel -> issueKernel.getKind()== Issue.IssueType.FIELD;
+
+    public static final Predicate<IssueKernel> NOTSHADED = issueKernel -> !issueKernel.getClassName().startsWith("org.springframework.asm.");
+
+    public static final Map<String,Predicate<IssueKernel>> FILTERS = new LinkedHashMap(){{
+        put("all", ALL);
+        put("arg", ARG);
+        put("ret", RET);
+        put("fld", FLD);
+    }};
 
     public static void main (String[] args) throws IOException {
 
-        Preconditions.checkArgument(EXTRACTED_PLUS_ISSUES_FOLDER.exists());
-        Preconditions.checkArgument(EXTRACTED_PLUS_ISSUES_FOLDER.isDirectory());
-        Preconditions.checkArgument(OBSERVED_PLUS_ISSUES_FOLDER.exists());
-        Preconditions.checkArgument(OBSERVED_PLUS_ISSUES_FOLDER.isDirectory());
-        Preconditions.checkArgument(SHADING_SPECS.exists());
-
+        Preconditions.checkArgument(EXTRACTED_FOLDER.exists());
+        Preconditions.checkArgument(OBSERVED_FOLDER.exists());
         new SampleDiff().analyse();
     }
 
     public void analyse()  {
 
-        // read shading specs
-        Set<ShadingSpec> shadingSpecs = this.readShadingSpecs(SHADING_SPECS);
-        Predicate<? extends AbstractIssue> shaded =
-            issue -> shadingSpecs.stream().anyMatch(spec -> issue.getClassName().startsWith(spec.getRenamed()));
+        Set<Issue> extractedIssues = (Set<Issue>) readIssues(EXTRACTED_FOLDER, MODULE, false);
+        Set<Issue> observedIssues = (Set<Issue>) readIssues(OBSERVED_FOLDER, MODULE, false);
 
-        for (String module: SPRING_MODULES) {
-            Set<Issue> extracted = (Set<Issue>) readIssues(EXTRACTED_PLUS_ISSUES_FOLDER, module, false);
-            Set<Issue> observed = (Set<Issue>) readIssues(OBSERVED_PLUS_ISSUES_FOLDER, module, false);
+        Map<IssueKernel,Set<Issue>> extractedIssueAggregation = IssueAggregator.aggregateAsMap(extractedIssues);
+        Map<IssueKernel,Set<Issue>> observedIssueAggregation = IssueAggregator.aggregateAsMap(observedIssues);
 
-            Map<IssueKernel,Set<Issue>> extractedAggregated = IssueAggregator.aggregateAsMap(extracted);
-            Map<IssueKernel,Set<Issue>> observedAggregated = IssueAggregator.aggregateAsMap(observed);
+        printDataset("extracted",extractedIssueAggregation.keySet());
+        printDataset("observed",observedIssueAggregation.keySet());
 
-            Set<IssueKernel> extractedMinusObservedAggregated = Sets.difference(extractedAggregated.keySet(),observedAggregated.keySet());
-            Set<IssueKernel> observedMinusExtractedAggregated = Sets.difference(observedAggregated.keySet(),extractedAggregated.keySet());
-            Set<Issue> extractedMinusObserved = extractedMinusObservedAggregated.stream()
-                .flatMap(kernel -> extractedAggregated.get(kernel).stream())
-                .collect(Collectors.toSet());
-            Set<Issue> observedMinusExtracted = observedMinusExtractedAggregated.stream()
-                .flatMap(kernel -> observedAggregated.get(kernel).stream())
-                .collect(Collectors.toSet());
+        System.out.println("extracted (all): " + extractedIssueAggregation.size());
+        System.out.println("observed (all): " + observedIssueAggregation.size());
 
-            File fileExtractedMinusObserved = new File(EXTRACTED_NOT_OBSERVED.replace("<module>",module));
-            IssuePersistency.save(extractedMinusObserved,fileExtractedMinusObserved);
+        Set<IssueKernel> xtrMinusObs = Sets.difference(extractedIssueAggregation.keySet(),observedIssueAggregation.keySet());
+        Set<IssueKernel> obsMinusXtr= Sets.difference(observedIssueAggregation.keySet(),extractedIssueAggregation.keySet());
 
-            File fileObservedMinusExtracted = new File(OBSERVED_NOT_EXTRACTED.replace("<module>",module));
-            IssuePersistency.save(observedMinusExtracted,fileObservedMinusExtracted);
+        // by type
+        printDataset("extracted-observed",xtrMinusObs);
+        printDataset("observed-extracted",obsMinusXtr);
 
+        System.out.println("exporting interesting diffs ");
+        writeIssues(obsMinusXtr.stream().filter(RET).filter(NOTSHADED).collect(Collectors.toList()), observedIssueAggregation,"observed-minus-extracted-return");
+        writeIssues(obsMinusXtr.stream().filter(FLD).filter(NOTSHADED).collect(Collectors.toList()), observedIssueAggregation,"observed-minus-extracted-fields");
+        writeIssues(obsMinusXtr.stream().filter(ARG).filter(NOTSHADED).collect(Collectors.toList()), observedIssueAggregation,"observed-minus-extracted-args");
+
+    }
+
+    private static void writeIssues(Collection<IssueKernel> selected, Map<IssueKernel,Set<Issue>> aggregation, String fileName) {
+        writeIssuesToJSON(selected,aggregation,new File(fileName+".json"));
+        writeIssuesToMarkdown(selected,aggregation,new File(fileName+".md"));
+    }
+    private static void writeIssuesToJSON(Collection<IssueKernel> selected, Map<IssueKernel,Set<Issue>> aggregation, File file) {
+        List<Issue> obsMinusXtrReturnIssues = selected.stream()
+            .sorted(Comparator.comparing(IssueKernel::getClassName))
+            .flatMap(kernel -> aggregation.get(kernel).stream().limit(ISSUE_INSTANCE_PER_KERNEL_LIMIT))
+            .collect(Collectors.toList());
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter out = new FileWriter(file)) {
+            gson.toJson(obsMinusXtrReturnIssues,out);
+            System.out.println("issues exported to " + file.getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    private static void writeIssuesToMarkdown(Collection<IssueKernel> selected, Map<IssueKernel,Set<Issue>> aggregation, File file) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(file))){
+            selected.stream()
+                .sorted(Comparator.comparing(IssueKernel::getClassName))
+                .forEach(issueKernel -> {
+
+                    String params = null;
+                    if (issueKernel.getKind() == Issue.IssueType.FIELD) {
+                        params = DescriptorParser.parseFieldDescriptor(issueKernel.getDescriptor());
+                    }
+                    else {
+                        params = DescriptorParser.parseMethodDescriptor(issueKernel.getDescriptor()).getParamTypes().stream().collect(Collectors.joining(",","(",")"));
+                    }
+
+                    String pos = "";
+                    if (issueKernel.getKind()==Issue.IssueType.RETURN_VALUE) {
+                        pos = "return type of";
+                    }
+                    else if (issueKernel.getKind()==Issue.IssueType.ARGUMENT) {
+                        pos = "argument #" + issueKernel.getArgsIndex() + " of";
+                    }
+
+                    out.println("### Add `@Nullable` to " + pos + "`" + issueKernel.getClassName() + "::" + issueKernel.getMethodName() + params );
+                    out.println("");
+                    out.println("Supporting test traces illustrating the use of null in actual program behaviour (max " + ISSUE_INSTANCE_PER_KERNEL_LIMIT + " is shown): ");
+                    aggregation.get(issueKernel).stream().limit(ISSUE_INSTANCE_PER_KERNEL_LIMIT).forEach(
+                        issue -> {
+                            out.println("```");
+                            for (String line:issue.getStacktrace()) {
+                                out.println(line);
+                            }
+                            out.println("```");
+                        }
+                    );
+                    out.println("");
+                    }
+                );
+
+            out.println();
+            out.println("A description of the analysis performed can be found here: (https://github.com/jensdietrich/null-annotation-inference)");
+            System.out.println("issues exported to " + file.getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printDataset(String name, Set<IssueKernel> dataset) {
+        for (String filterName: FILTERS.keySet()) {
+            Predicate<IssueKernel> filter = FILTERS.get(filterName);
+            System.out.print(name + " (" + filterName + "): ");
+            System.out.println(dataset.stream().filter(filter).count());
+        }
+        System.out.println();
+    }
+
+
 
 }
